0a551f38b1080488c1b3668705d9e3017d2f4ced
Vanderwerff, Kelly <Kelly.Vanderwerff@POET.COM>
2025-10-23 12:36:59 -0500

    make two assets and use them in tasks

--- 01_getting_started.py
+++ 01_getting_started.py
@@ -5,34 +5,36 @@ import csv
 import io, urllib.request
 
 input_file = "https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/22b7a28fb8ac31303a6350702f26bc75c93d5551/example_input.csv"
-output_file = "https://gist.github.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/example_output.csv"
+output_file = "https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/22b7a28fb8ac31303a6350702f26bc75c93d5551/example_output.csv"
 
-
-@materialize(input_file)
-def extract_data() -> str:
+def get_data(filename):
     data_extract = []
     with urllib.request.urlopen(input_file) as resp:
         text = resp.read().decode("utf-8")
         reader = csv.DictReader(io.StringIO(text))
         data_extract = list(reader)
+    return data_extract 
+
+
+@materialize(input_file)
+def extract_input_data() -> str:
+    data_extract = get_data(input_file)
     return data_extract
+    
 
 @materialize(output_file)
-def load_data(data_extract):
-    with open(output_file, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(data_extract)
-    return
+def extract_output_data() -> str:
+    data_extract = get_data(output_file)
+    return data_extract
 
 @task
 def get_customer_ids() -> list[str]:
-    # Fetch customer IDs from a database or API
-    return [f"customer{n}" for n in random.choices(range(100), k=50)]
+    return extract_input_data()
 
 @task
 def process_customer(customer_id: str) -> str:
     # Process a single customer
-    return f"Processed {customer_id}"
+    return extract_output_data()
 
 #@flow
 #def main() -> list[str]:
