118eb41c13091438c02f54140b2058d821cd0f9b
Vanderwerff, Kelly <Kelly.Vanderwerff@POET.COM>
2025-10-24 13:24:16 -0500

    start of demo for user group

---
+++ bakery_demo.py
@@ -0,0 +1,65 @@
+from prefect import flow, task
+from prefect.assets import materialize 
+import random
+import csv
+import io, urllib.request
+import json
+from prefect_github import GitHubCredentials
+
+TOKEN = GitHubCredentials.load("github")
+
+gist_url = f"https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/546e4d49d06d0d1e839080afaaee26247ea4cb5f"
+customer_data = "https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/546e4d49d06d0d1e839080afaaee26247ea4cb5f/customers.csv"
+product_data = "https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/546e4d49d06d0d1e839080afaaee26247ea4cb5f/products.csv"
+transaction_data = "https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/546e4d49d06d0d1e839080afaaee26247ea4cb5f/transactions.csv"
+customer_analytics = "https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/546e4d49d06d0d1e839080afaaee26247ea4cb5f/dollars_by_customer.csv"
+product_analytics = "https://gist.githubusercontent.com/kellyvdw/800e7bf7c06028a0d4e74539834e05a1/raw/546e4d49d06d0d1e839080afaaee26247ea4cb5f/dollars_by_product.csv"
+
+def get_data(filename):
+    data_extract = []
+    with urllib.request.urlopen(filename) as resp:
+        text = resp.read().decode("utf-8")
+        data_extract = csv.DictReader(io.StringIO(text))
+        #data_extract = list(reader)
+    return data_extract 
+
+def write_data(filename, data_out, gist_url):
+
+    update_payload = {
+    "files": {
+        filename: {"content": data_out}
+        }
+    }
+
+    req = urllib.request.Request(
+        gist_url,
+        data=json.dumps(update_payload).encode("utf-8"),
+        headers={
+            "Authorization": f"token {TOKEN}",
+            "User-Agent": "python-urllib",
+            "Content-Type": "application/json"
+        },
+        method="PATCH"
+    )
+
+    with urllib.request.urlopen(req) as resp:
+        gist_info = json.load(resp)
+        print("Updated raw URL:", gist_info["files"][filename]["raw_url"])
+    return
+   
+@task
+def get_customer_data(customer_data):
+    data_extract = get_data(customer_data)
+    return data_extract
+
+@flow
+def main():
+    customers = get_customer_data(customer_data)
+    return customers
+
+
+
+if __name__ == "__main__":
+    main.serve(
+        name="bakery_analytics_deployment"
+        )
